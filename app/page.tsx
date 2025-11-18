b6567560d230ac7a7a812bed35f11f52902af772
Manpreet.Singh18 <manpreet.singh18@student.ufv.ca>
2025-11-18 12:40:13 -0800

    Updated Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 
 export default function Home(){
     return(
-        <h1>Welcome to Home page</h1>
+        <div>
+            <h1>Welcome to Home page</h1>
+            <h2>All your fav products in one place</h2>
+        </div>
     )
 }
