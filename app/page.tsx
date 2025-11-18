1dd391a8b4944985419941122dff5379333b3a54
Manpreet.Singh18 <manpreet.singh18@student.ufv.ca>
2025-11-18 12:08:11 -0800

    Added Auth

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+"use client"
+
+export default function Home(){
+    return(
+        <h1>Welcome to Home page</h1>
+    )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,38 @@
+"use client"
+import React from 'react';
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+import Link from 'next/link'
+import {House} from 'lucide-react'
+import {Button} from './ui/button'
+
+const Navbar = () =>{
+    return(
+        <div className='flex items-center justify-between'>
+            <Link href={'/'} className='ml-10 flex gap-2'>
+                <p><House /></p>
+                <p className='font-bold'>Home</p>
+            </Link>
+            <div className='flex items-center py-7 px-5 justify-end gap-5'>
+                <SignedOut>
+                    <Link href={'/sign-in'}>
+                        <Button>
+                            Get Started
+                        </Button>
+                    </Link>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton />
+                </SignedIn>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
